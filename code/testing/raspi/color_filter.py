5165c282c0977adfbf300ef478e43ddb120798a9
nikoro1904 <niko.rosskopf@t-online.de>
2022-08-30 10:50:34 +0200

    updates

--- code/testing/raspi/color_filter.py
+++ code/testing/raspi/color_filter.py
@@ -6,10 +6,18 @@ upper_yellow = np.array([64, 255, 255])
 
 capture = cv.VideoCapture(0)
 
+time_filter_len = 5
+frames = []
+
 while True:
     isTrue, frame = capture.read()
 
     if isTrue:
+        frames.append(frame)
+        if len(frames) > time_filter_len:
+            frames = frames[-time_filter_len:]
+        time_filtered_frame = np.round(np.mean(np.array(frames),axis=0)).astype(int)
+
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
         lower_blue = np.array([60, 35, 140])
@@ -18,6 +26,7 @@ while True:
         mask = cv.inRange(hsv, lower_yellow, upper_yellow)
         result = cv.bitwise_and(frame, frame, mask = mask)
         
+        cv.imshow("time_filtered", time_filtered_frame)
         cv.imshow("mask", mask)
         cv.imshow("frame", frame)
         cv.imshow('result', result)
